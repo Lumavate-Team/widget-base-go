3ffb569c52e725adaf0872ec58c13847ce42af58
Jack Eberhardt <jackeberhardt@Jacks-MacBook-Pro.local>
2018-05-10 11:19:01 -0400

    brought in signer and lumavate request

--- widget/controllers/default.go
+++ widget/controllers/default.go
@@ -1,73 +1,24 @@
 package controllers
 
 import (
-  "github.com/astaxie/beego"
-  signer "github.com/Lumavate-Team/go-signer"
-  "github.com/Lumavate-Team/go-properties/component_data"
-  "fmt"
-  "os"
-  "net/http"
-  "io/ioutil"
+  common_controller "github.com/Lumavate-Team/lumavate-go-common"
   "encoding/json"
+  "widget/models"
 )
 
 type MainController struct {
-  beego.Controller
-}
-
-type LumavateRequest struct {
-  Payload struct {
-    Data struct {
-      PageType component_data.PageTypeStruct
-      Quote component_data.QuoteStruct
-      SampleText string
-      NavBar component_data.NavBarStruct
-      NavBarItems [] component_data.NavBarItemStruct
-    }
-  }
+  common_controller.LumavateController
 }
 
 func (this *MainController) Get() {
-  pwa_jwt := this.Ctx.GetCookie("pwa_jwt")
+  luma_response := models.LumavateRequest {}
+  err := json.Unmarshal(this.LumavateGetData(), &luma_response)
 
-  no_auth_redirect_url := fmt.Sprintf("%s%s?u=/%s/%s/%s",
-    os.Getenv("PROTO"),
-    this.Ctx.Input.Host(),
-    this.Ctx.Input.Param(":ic"),
-    this.Ctx.Input.Param(":url_ref"),
-    this.Ctx.Input.Param(":wid"),
-    )
-
-  widget_data_url := fmt.Sprintf("/pwa/v1/widget-instances/%s",
-    this.Ctx.Input.Param(":wid"),
-    )
-
-  s := signer.Signer{}
-
-  signed_widget_data_url := fmt.Sprintf("%s%s",
-    os.Getenv("BASE_URL"),
-    s.GetSignature("get", widget_data_url, []byte{}))
-
-  req, _ := http.NewRequest("GET", signed_widget_data_url, nil)
-  req.Header.Add("Content-Type", "application/json")
-  req.Header.Add("Authorization", "Bearer " + pwa_jwt)
-
-  res, _ := http.DefaultClient.Do(req)
-
-  defer res.Body.Close()
-  body, _ := ioutil.ReadAll(res.Body)
-
-  if res.StatusCode == 401 {
-    this.Ctx.Redirect(302, no_auth_redirect_url)
-  } else if res.StatusCode == 403 {
-    this.Abort("403")
-  } else if res.StatusCode == 500 {
+  if err != nil {
     this.Abort("500")
   }
 
-  luma_response := LumavateRequest{}
-  json.Unmarshal(body, &luma_response)
-
+  luma_response.Payload.Data.NavBar.ComponentData.NavBarItems = luma_response.Payload.Data.NavBarItems
   this.Data["data"] = luma_response.Payload.Data
   this.TplName = "index.tpl"
 }
--- widget/main.go
+++ widget/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-  "github.com/Lumavate-Team/go-properties/component_data"
+  "github.com/Lumavate-Team/lumavate-go-common/properties/component_data"
 	_ "widget/routers"
 	"github.com/astaxie/beego"
   "html/template"
@@ -15,6 +15,7 @@ func ComponentHtml(in component_data.ComponentData) (out template.HTML){
 
 func main() {
 	beego.SetStaticPath(os.Getenv("WIDGET_URL_PREFIX") + "static","static")
+	beego.SetStaticPath(os.Getenv("WIDGET_URL_PREFIX") + "lc","/lumavate-components/dist")
 	beego.AddFuncMap("componentHtml", ComponentHtml)
 	beego.Run()
 }
